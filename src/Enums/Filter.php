cedee18156e2d8533899f4e19a696e1b37e32b70
AngryMoustache <sander.vandamme11@gmail.com>
2022-05-15 18:04:06 +0200

    Filter enum

---
+++ src/Enums/Filter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AngryMoustache\Predator\Enums;
+
+class Filter
+{
+    public const STRING_CONTAINS = 'string_contains';
+    public const STRING_IS = 'string_is';
+    public const ARRAY_ALL = 'arrays_all';
+    public const ARRAY_SOME = 'arrays_some';
+}
