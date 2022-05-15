8719cc1c6ebf0a8933f59d7bdb03ad6e6988050d
AngryMoustache <sander.vandamme11@gmail.com>
2022-05-15 15:50:10 +0200

    Init

---
+++ src/Providers/PredatorServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AngryMoustache\Predator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
